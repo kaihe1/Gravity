96513839eb8c564c516e587cfa1bd497a02f5b24
kaihe1 <42453853+kaihe1@users.noreply.github.com>
2019-04-10 11:42:07 -1000

    Add files via upload

--- jumper.js
+++ jumper.js
@@ -7,7 +7,7 @@ var cp;
     this.lift = -10; //opposing the force of gravity
     this.velocity = 0;
     this.show = function() {
-      rect (this.x,this.y,50,50);
+      rect (this.x,this.y,70,70);
       image(cp,this.x,this.y,70,70);
     }
     
