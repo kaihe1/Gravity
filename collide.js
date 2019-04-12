70d4202d9f55d574ea705ec98403d0a36731bb31
kaihe1 <42453853+kaihe1@users.noreply.github.com>
2019-04-12 11:54:55 -1000

    Add files via upload

--- collide.js
+++ collide.js
@@ -1,9 +1,9 @@
+var cop;
 function collide(){
   this.x = w/2;
   this.y = h/2;
   
   this.show = function(){
-    fill(color('green'));
-    rect(this.x,this.y,100,100);
+   image(cop,this.x,this.y,130,130);
   }
 }
