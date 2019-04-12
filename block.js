5e89ce6b092d228b597a74267ed752726d910ea8
kaihe1 <42453853+kaihe1@users.noreply.github.com>
2019-04-12 11:40:42 -1000

    Add files via upload

--- block.js
+++ block.js
@@ -22,3 +22,24 @@ var blockp;
     }
   
   }
+  
+  function block2() {
+    this.x = w;
+    this.y = 0;
+    this.gravity = 0.4;
+    this.velocity = 0;
+    this.show = function() {
+    image(blockp,this.x,this.y,170,85);
+    }
+    
+    this.updata = function() {
+      this.velocity += this.gravity;
+      this.x -= this.velocity;
+      if (this.x < 0) {
+        this.x = w;
+        this.velocity = 0;
+        this.y = Math.floor(Math.random()*h);
+      }
+    }
+  
+  }
