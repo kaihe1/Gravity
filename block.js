a92213f33631795ccd6ce7e89841568080a1104b
kaihe1 <42453853+kaihe1@users.noreply.github.com>
2019-04-12 11:27:06 -1000

    Add files via upload

--- block.js
+++ block.js
@@ -1,13 +1,11 @@
-
+var blockp;
   function block() {
     this.x = w;
     this.y = 0;
     this.gravity = 0.4;
     this.velocity = 0;
-    yr = 50;
     this.show = function() {
-      fill(color('white'));
-      rect(this.x,this.y,50,yr);
+    image(blockg,this.x,this.y,170,85);
     }
     
     this.updata = function() {
@@ -17,7 +15,6 @@
         this.x = w;
         this.velocity = 0;
         this.y = Math.floor(Math.random()*h);
-        yr = random(150,200);
         score ++;
         document.getElementById('score').innerHTML = "Score:" + score;
       }
